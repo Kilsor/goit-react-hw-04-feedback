66778588abe1c7ffc7332de3914a1261d52e4fa0
Kilsor <kilsors@gmail.com>
2023-09-10 12:50:23 +0300

    hw4.1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  // Крок 1: Створення стану для збереження відгуків
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  // Крок 2: Обробник для збільшення лічильника відгука обраного типу
+  const handleLeaveFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
   };
 
-  // Метод для підрахунку загальної кількості відгуків
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
+  // Крок 3: Обчислення загальної кількості відгуків та відсотка позитивних відгуків
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage =
+    totalFeedback === 0 ? 0 : Math.round((feedback.good / totalFeedback) * 100);
 
-  // Метод для підрахунку відсотка позитивних відгуків
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total === 0 ? 0 : Math.round((good / total) * 100);
-  }
+  const feedbackOptions = ['good', 'neutral', 'bad'];
 
-  // Обробник для кнопок відгуків
-  handleLeaveFeedback = feedback => {
-    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
-  };
+  return (
+    <div>
+      {/* Крок 4: Відображення секції для залишення відгуків */}
+      <Section title="Please leave your feedback">
+        {/* Крок 5: Відображення компонента FeedbackOptions */}
+        <FeedbackOptions
+          options={feedbackOptions}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+      </Section>
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    const feedbackOptions = ['good', 'neutral', 'bad'];
-
-    return (
-      <div>
-        {/* Секція для залишення відгуків */}
-        <Section title="Please leave your feedback">
-          <FeedbackOptions
-            options={feedbackOptions}
-            onLeaveFeedback={this.handleLeaveFeedback}
+      {/* Крок 6: Умовний рендерінг для відображення статистики або повідомлення */}
+      {totalFeedback > 0 ? (
+        /* Крок 7: Відображення секції для відображення статистики */
+        <Section title="Statistics">
+          {/* Крок 8: Відображення компонента Statistics та передача йому необхідних даних */}
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
           />
         </Section>
-
-        {/* Умовна рендерінг для відображення статистики або повідомлення */}
-        {totalFeedback > 0 ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positivePercentage}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
+      ) : (
+        /* Крок 9: Відображення повідомлення, якщо немає відгуків */
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
 }
