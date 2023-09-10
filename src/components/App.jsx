41834b3ba80b8a9653ee4dbdc26e0414ba4bfef2
Kilsor <kilsors@gmail.com>
2023-09-10 12:58:46 +0300

    hw4.1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
